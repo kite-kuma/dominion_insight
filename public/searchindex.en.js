7038f9b0db248e327844d54889f9dae0b619706a
kite <sgwgkaito@gmail.com>
2025-02-25 00:53:55 +0900

    steroid

---
+++ public/searchindex.en.js
@@ -0,0 +1,66 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Dominion Insight",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Articles",
+    "uri": "/dominion_insight/articles/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Dominion Insight",
+    "uri": "/dominion_insight/index.html"
+  },
+  {
+    "breadcrumb": "Dominion Insight \u003e  Articles \u003e  戦略",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "ステロ",
+    "uri": "/dominion_insight/articles/strategies/steroid/index.html"
+  },
+  {
+    "breadcrumb": "Dominion Insight \u003e  Articles \u003e  戦略 \u003e  ステロ",
+    "content": "ドミニオンにはステロとコンボがある．これは幾度となく言われることであるが，実際にドミニオンの 2 人戦で純粋なステロをする機会は多くない．基本的にはコンボに負けるためである．\nしかし完全にランダムに選択したサプライにおいて，コンボができない，またはステロに比べて遅いものは無視できない程度にはある．\nこのサイトでは様々なカードを用いるステロについて検証し，コンボや他のステロとの比較検証を行っていくこととする．",
+    "description": "ドミニオンにはステロとコンボがある．これは幾度となく言われることであるが，実際にドミニオンの 2 人戦で純粋なステロをする機会は多くない．基本的にはコンボに負けるためである．\nしかし完全にランダムに選択したサプライにおいて，コンボができない，またはステロに比べて遅いものは無視できない程度にはある．\nこのサイトでは様々なカードを用いるステロについて検証し，コンボや他のステロとの比較検証を行っていくこととする．",
+    "tags": [],
+    "title": "ステロの基本",
+    "uri": "/dominion_insight/articles/strategies/steroid/steroid_basic/index.html"
+  },
+  {
+    "breadcrumb": "Dominion Insight \u003e  Articles",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "戦略",
+    "uri": "/dominion_insight/articles/strategies/index.html"
+  },
+  {
+    "breadcrumb": "Dominion Insight \u003e  Articles",
+    "content": "はじめに このサイトではデッキ構築型カードゲーム『ドミニオン』の戦術について解説します．主にドミニオンオンラインにおける 2 人対戦を想定しています．\n管理人について ドミニオン歴自体は 4 年ほどで，ドミニオンオンラインでレートが 58 程度です．ドミニオンの基礎的な戦術について概ね理解していると思っています．一方で構築手順や終盤での立ち回りなど，まだまだ最上位層には実力が遠い部分もあると感じています．\n私自身が日々ドミニオンに触れていく中で得た学びを文章化することで実力向上にも繋がるのではと思い，このサイトを制作しました．よろしくお願いします．",
+    "description": "はじめに このサイトではデッキ構築型カードゲーム『ドミニオン』の戦術について解説します．主にドミニオンオンラインにおける 2 人対戦を想定しています．\n管理人について ドミニオン歴自体は 4 年ほどで，ドミニオンオンラインでレートが 58 程度です．ドミニオンの基礎的な戦術について概ね理解していると思っています．一方で構築手順や終盤での立ち回りなど，まだまだ最上位層には実力が遠い部分もあると感じています．\n私自身が日々ドミニオンに触れていく中で得た学びを文章化することで実力向上にも繋がるのではと思い，このサイトを制作しました．よろしくお願いします．",
+    "tags": [],
+    "title": "このサイトについて",
+    "uri": "/dominion_insight/articles/intro/index.html"
+  },
+  {
+    "breadcrumb": "Dominion Insight",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/dominion_insight/categories/index.html"
+  },
+  {
+    "breadcrumb": "Dominion Insight",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/dominion_insight/tags/index.html"
+  }
+]
